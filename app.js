ad572d455028db217fa1e0bb0afd1def6a224ab6
brenda-soto <bsoto@pursuit.org>
2024-04-17 15:26:13 -0400

    Fixed netlify url

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ cron.schedule("*/5 * * * *", () => {
 app.use(
   cors({
     // origin: "http://localhost:3000",
-    origin: "https://fibercreations.netlify.app/",
+    origin: "https://fibercreations.netlify.app",
   })
 );
 app.use(express.json());
